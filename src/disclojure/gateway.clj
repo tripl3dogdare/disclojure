23b644e19527ae3503d3102bda3be80d6da716c6
Connor Scialdone <connor@connorjulian.org>
2018-01-09 16:44:10 -0700

    Add documentation to gateway.clj

--- src/disclojure/gateway.clj
+++ src/disclojure/gateway.clj
@@ -1,4 +1,5 @@
 (ns disclojure.gateway
+  "Contains the main gateway implementation for Disclojure."
   (:require
     [clojure.data.json :as json]
     [http.async.client :as http]
@@ -12,13 +13,21 @@
   set-heartbeat)
 
 (def gateway-endp "https://discordapp.com/api/v6/gateway")
-(def gateway-url (str
-  (get (with-open [client (http/create-client)]
-    (let [res (http/GET client gateway-endp)]
-      (-> res http/await http/string json/read-str))) "url")
-  "?v=6&encoding=json"))
+(def gateway-url
+  "Retrieves the URL that should be used to connect to the gateway."
+  (str
+    (get (with-open [client (http/create-client)]
+      (let [res (http/GET client gateway-endp)]
+        (-> res http/await http/string json/read-str))) "url")
+    "?v=6&encoding=json"))
 
 (defn on-receive
+  "Called on receiving a message from the websocket.
+   Handles:
+     - Event sequence tracking
+     - Dispatching events to the function given in the session's :dispatch key
+     - Setting up and maintaining heartbeats
+     - Receiving and processing reconnect and invalid session messages."
   [session ws msg]
   (let
     [ event (json/read-str msg :key-fn keyword)
@@ -48,6 +57,7 @@
       :invalid-session (reconnect session data))))
 
 (defn connect
+  "Creates a new session and connects to the gateway."
   ([token] (connect token false))
   ([token resume?]
     (connect token resume? { :dispatch (fn [& _] ()) }))
@@ -77,11 +87,14 @@
       session)))
 
 (defn disconnect [session]
+  "Disconnects the given session."
   (sset session :connected? false)
   (http/close (@session :socket))
   (future-cancel (@session :heartbeat-timer)))
 
 (defn reconnect
+  "Attempts to reconnect the given session.
+   Attempts to resume by default, otherwise reidentifies from scratch."
   ([session] (reconnect session true))
   ([session resume?]
     (let
@@ -95,6 +108,8 @@
         (connect token)))))
 
 (defn set-heartbeat
+  "Creates a timer to send heartbeats at the proper intervals.
+   Checks for acks and attempts to reconnect if the last ack was not recieved."
   ([session] (set-heartbeat session false))
   ([session now?]
     (set-interval
@@ -106,6 +121,7 @@
       (@session :heartbeat-int) now?)))
 
 (def gate-ops
+  "A map from raw numeric opcodes to their keyword equivalents."
   { 0 :dispatch
     1 :heartbeat
     2 :identify
@@ -118,29 +134,46 @@
     9 :invalid-session
     10 :hello
     11 :ack })
-(def get-op (map-invert gate-ops))
+(def get-op
+  "A map from keyword opcodes to their raw numeric equivalents."
+  (map-invert gate-ops))
 
-(defn packet [op d] {:op (get-op op) :d d})
-(defn identify [session] (packet :identify {
-  :token (@session :token)
-  :properties {
-    :os (System/getProperty "os.name")
-    :device "disclojure"
-    :browser "disclojure" }
-  :compress false }))
-(defn resume [session] (packet :resume {
-  :token (@session :token)
-  :session_id (@session :sid)
-  :seq (@session :seq) }))
+(defn- packet
+  "Wraps the given opcode and data in standard packet structure."
+  [op d]
+  {:op (get-op op) :d d})
+(defn- identify
+  "Creates a new identify packet for the given session."
+  [session]
+  (packet :identify {
+    :token (@session :token)
+    :properties {
+      :os (System/getProperty "os.name")
+      :device "disclojure"
+      :browser "disclojure" }
+    :compress false }))
+(defn- resume
+  "Creates a new resume packet for the given session."
+  [session]
+  (packet :resume {
+    :token (@session :token)
+    :session_id (@session :sid)
+    :seq (@session :seq) }))
 
 (defn set-interval
+  "Utility function for running a function at set intervals.
+   If now? is true, the function will be run immediately the first time."
   ([callback ms] (set-interval callback ms false))
   ([callback ms now?]
     (future (while true (do
       (if (not now?) (Thread/sleep ms))
       (callback)
       (if now? (Thread/sleep ms)))))))
-(defn ws-send [socket msg]
+(defn ws-send
+  "Converts the given message to JSON and sends it over the given socket."
+  [socket msg]
   (http/send socket :text (json/write-str msg)))
-(defn sset [session & args]
+(defn sset
+  "Utility function for updating fields on the given session."
+  [session & args]
   (apply (partial swap! session assoc) args))
