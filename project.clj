971e0b06c7a8c37adaec30da76a45f3fb36fbb4a
Connor Scialdone <connor@connorjulian.org>
2018-01-10 14:55:40 -0700

    Update project.clj to include Codox documentation plugin

--- project.clj
+++ project.clj
@@ -9,6 +9,13 @@
     [org.clojure/core.async "0.3.465"]
     [http.async.client "1.2.0"]]
   :resource-paths ["resources/slf4j-nop-1.7.25.jar"]
+  :plugins [[lein-codox "0.10.3"]]
+  :codox {
+    :namespaces [#"^disclojure\."]
+    :metadata {
+      :doc "FIXME: Document this!" }
+    :output-path "docs"
+    :source-uri "https://github.com/tripl3dogdare/disclojure/blob/master/{filepath}#L{line}" }
   :profiles {
     :dev {
       :source-paths ["example"]
