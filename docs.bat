709d380ad68e65d37b58de534cc58101b79bce74
Connor Scialdone <connor@connorjulian.org>
2018-01-16 15:48:22 -0700

    Modify docs.bat to prompt before pushing documentation

--- docs.bat
+++ docs.bat
@@ -1,3 +1,12 @@
 @echo off
+echo Generating documentation...
 cmd /c "lein codox"
-ftp -s:docs.ftp -i -v
+
+set upl=y
+set /p upl="Upload? (Y/n) "
+
+set uplb=1
+if %upl:~0,1%==n ( set uplb=0 )
+if %upl:~0,1%==N ( set uplb=0 )
+
+if %uplb%==1 ( ftp -s:docs.ftp -i -v )
